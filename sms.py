822bdeb41f83439a939b4e5d23a078fd89a079ad
John Doe <davor.kos@gmail.com>
2024-07-16 15:44:30 +0200

    završena forma za unos podataka

--- sms.py
+++ sms.py
@@ -6,24 +6,81 @@ import ttkthemes
 
 
 def add_student():
-    addWindow = Toplevel()
-    addWindow.grab_set()  # ako kliknem izvan prozora neće pasti iza
-    addWindow.geometry('480x250+730+230')
-    addWindow.title('Add student')
-    addWindow.resizable(False, False)
+    def add_data():
+        if (idEntry.get() == '' or nameEntry.get() == '' or phoneEntry.get() == '' or emailEntry.get() == ''
+                or addressEntry.get() == '' or genderEntry.get() == '' or dobEntry.get() == ''):
+            messagebox.showerror('Error', 'All fields are required!!!', parent=add_window)
+        else:
+            currentdate = time.strftime('%d.%m.%Y')
+            currenttime = time.strftime('%H:%M:%S')
+            query = 'insert into student values(%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+            mycursor.execute(query,
+                             (idEntry.get(), nameEntry.get(), phoneEntry.get(), emailEntry.get(), addressEntry.get(),
+                              genderEntry.get(), dobEntry.get(), currentdate, currenttime))
+            conn.commit()  # execute zapis prem bazi
+            result = messagebox.askyesno('Data Entry',
+                                         'Data added successfully. Do you want to clean the form')  # oš očistiti formu?
+            if result:
+                idEntry.delete(0, END)
+                nameEntry.delete(0, END)
+                phoneEntry.delete(0, END)
+                emailEntry.delete(0, END)
+                addressEntry.delete(0, END)
+                genderEntry.delete(0, END)
+                dobEntry.delete(0, END)
 
-    idLabel = Label(addWindow, text='Id', font=('arial', 20))
-    idLabel.grid(row=0, column=0, padx=30, pady=20)
-    idLabelEntry = Entry(addWindow, font=('Helvetica', 15, 'bold'), bd=2)
-    idLabelEntry.grid(row=0, column=1, pady=20)
+    add_window = Toplevel()
+    add_window.grab_set()  # ako kliknem izvan prozora neće pasti iza
+    add_window.resizable(False, False)
+    add_window.geometry('480x550+730+230')
+    add_window.title('Add student')
+    add_window.resizable(False, False)
+
+    idLabel = Label(add_window, text='Id', font=('arial', 20))
+    idLabel.grid(row=0, column=0, padx=30, pady=15, sticky="w")
+    idEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    idEntry.grid(row=0, column=1, pady=15)
+
+    nameLabel = Label(add_window, text='Name', font=('arial', 20))
+    nameLabel.grid(row=1, column=0, padx=30, pady=15, sticky="w")
+    nameEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    nameEntry.grid(row=1, column=1, pady=15)
+
+    phoneLabel = Label(add_window, text='Phone', font=('arial', 20))
+    phoneLabel.grid(row=2, column=0, padx=30, pady=15, sticky="w")
+    phoneEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    phoneEntry.grid(row=2, column=1, pady=15)
+
+    emailLabel = Label(add_window, text='Email', font=('arial', 20))
+    emailLabel.grid(row=3, column=0, padx=30, pady=15, sticky="w")
+    emailEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    emailEntry.grid(row=3, column=1, pady=15)
+
+    addressLabel = Label(add_window, text='Address', font=('arial', 20))
+    addressLabel.grid(row=4, column=0, padx=30, pady=15, sticky="w")
+    addressEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    addressEntry.grid(row=4, column=1, pady=15)
+
+    genderLabel = Label(add_window, text='Gender', font=('arial', 20))
+    genderLabel.grid(row=5, column=0, padx=30, pady=15, sticky="w")
+    genderEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    genderEntry.grid(row=5, column=1, pady=15)
+
+    dobLabel = Label(add_window, text='DOB', font=('arial', 20))
+    dobLabel.grid(row=6, column=0, padx=30, pady=15, sticky="w")
+    dobEntry = Entry(add_window, font=('Helvetica', 15, 'bold'), bd=2)
+    dobEntry.grid(row=6, column=1, pady=15)
+
+    add_student_button = ttk.Button(add_window, text="Add Student", command=add_data)
+    add_student_button.grid(row=7, column=1)
 
 
 def connect_database():
     def connect():
+        global mycursor, conn
         try:
             # conn = pymysql.connect(host=hostnameEntry.get(), user=usernameEntry.get(), password=passwordEntry.get())
             conn = pymysql.connect(host='localhost', user='root', password='toor')
-
             mycursor = conn.cursor()
         except:
             messagebox.showerror('Error', 'Invalid access', parent=connectWindow)
