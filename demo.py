d2ca74899c9a27791d138ace8cd7184951918c08
Davor <davor.kos@gmail.com>
2024-08-21 00:08:12 +0200

    sync

--- demo.py
+++ demo.py
@@ -1,7 +1,17 @@
-# 140. Writing & Appending Data To A File
-file = open('data.txt', 'w')
-file.write('Novi sadrzaj u fileu')
-file.close()
+# 142. Readline & Readlines
+
+
+# 141. Opening Files Using With
+# with se koristi za otvraanje fileova i zatvaranje da ne moramo koristiti file.close svaki put
+# with open('data.txt', 'r') as file:
+#     contents = file.read()
+#     print(contents)
+
+"""# 140. Writing & Appending Data To A File
+file = open('data.txt', 'a')
+content = ('\n5. Novi sadrzaji u fileu')
+file.write(content)
+file.close()"""
 
 #
 """# Opening A File
