775e0c267f3c496b29748a367c853863a74cdb98
Davor <davor.kos@gmail.com>
2024-08-21 22:45:27 +0200

    149

--- demo.py
+++ demo.py
@@ -1,5 +1,32 @@
 # 149. Writing Serialised Data To File
+import json
 
+
+def save_user_data():
+    user_list = []
+
+    while True:
+        name = input('Enter name (or q to quit: ')
+        if name == 'q':
+            break
+        email = input('Enter email: ')
+        contact = input('Enter contact: ')
+
+        # creatind dictionary
+        user_data = {
+            "name": name,
+            "email": email,
+            "contact": contact
+        }
+        # save any user data
+        user_list.append(user_data)
+
+        with open('user_data.json', 'w') as file:
+            json.dump(user_list, file)  # zabi json u user_list
+        print('User data saved succesfully')
+
+
+save_user_data()
 
 """# 148. Deserialization
 json_data = '{"name": "John Doe", "age": 30, "city": "New York"}'
