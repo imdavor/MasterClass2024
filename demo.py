ace1dcf2fa87fa92d902aa63a8fa71ff1ca9aa31
John Doe <davor.kos@gmail.com>
2024-08-21 15:55:50 +0200

    do 149

---
+++ demo.py
@@ -0,0 +1,17 @@
+# 149. Writing Serialised Data To File
+
+
+"""# 148. Deserialization
+json_data = '{"name": "John Doe", "age": 30, "city": "New York"}'
+data = json.loads(json_data)
+print(data)
+"""
+
+"""# 147. Serialization
+
+data = {"name": 'John Doe',
+        'age': 30,
+        'city': 'New York'}
+json_data = json.dumps(data)  # konvertira podatke u json string
+print(json_data)
+"""
