3cc0b61184d2a847b022d088d619a6f2209463d3
Davor <davor.kos@gmail.com>
2024-11-18 14:04:16 +0100

    demo 247 - 3

--- demo-247-BuildingCalculatorAppPart3.py
+++ demo-247-BuildingCalculatorAppPart3.py
@@ -5,19 +5,29 @@ from tkinter import *
 root = Tk()
 # create display
 display = Entry(root)
-root.geometry("200x200")
+# root.geometry("200x200")
 display.grid(row=1, columnspan=6)
 
-counter = 1
+i=0 # postavljamo index na 0
+def get_number(num):
+    global i
+    display.insert(i, num) # index(pozicija broja u displayu) broja je i, proslijedi num u display
+    i+=1 # svaki put kada je dodan broj povećaj poziciju indexa za 1
+
+
+# kako bi se brojevi prikazali u displayu izrađujemo array sa brojevima od 1-9 tako da funkcija get_number() može biti pozvana za svaki broj koji je kliknut
+numbers = [1,2,3,4,5,6,7,8,9]
+counter = 0 #postavljamo counter na 0 jer želimo da se brojevi prikazuju od 0-9
 # dodajmo 3x3 grid i buttone
 for x in range(3):
     for y in range(3):
-        button= Button(root, text=counter, width=4,height=2) #counter=brojevi od 0-9
+        button_text = numbers[counter] # svaki button loop kroz numbers
+        button= Button(root, text=button_text, width=4,height=2, command=lambda text=button_text:get_number(text)) #counter=brojevi od 0-9; ubacujemo lambda 
         button.grid(row=x+2, column=y) # kreiraj grid, x+2 koji će početi u 2.redu jer je 1. red rezerviran za display
         counter += 1 
 
 #button nula
-buttonzero= Button(root, text="0", width=4, height=2)
+buttonzero= Button(root, text="0", width=4, height=2, command=lambda :get_number(0))
 buttonzero.grid(row=5, column=1)
 
 # kreiramo listu operacija koje će raditi
