491358a815c1910eb57416fc9f171e940504d77e
Davor <davor.kos@gmail.com>
2024-11-24 12:54:03 +0100

    demo-calculator gotov

--- demo-calculator.py
+++ demo-calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import ast  # apstract syntax tree, pass any expression - sam kuži kaj treba napraviti
 
 
 root = Tk()
@@ -23,8 +24,41 @@ def get_operation(operator):
     i += 1  # dižemo poziciju za 1
 
 
+def clear_all():
+    display.delete(0, END)
+
+
+def calculate():
+    entire_string = display.get()  # 1. pokupi sve kaj je upisano u prozoru
+    try:
+        node = ast.parse(entire_string, mode="eval")  # stavi u var node
+        result = eval(
+            compile(node, "<string>", "eval")
+        )  # 2. proslijedi upisano(node) u ast i evaluiraj string i stavi sve u var result
+        clear_all()  # 3. očisti display
+        display.insert(
+            0, result
+        )  # 4. ubaci evaluirani result u display na poziciju index 0
+    except Exception:
+        clear_all()
+        display.insert(0, "Error")
+
+
+def undo():
+    entire_string = display.get()
+    if len(entire_string):  # provjera da display nije prazan
+        new_string = entire_string[
+            :-1
+        ]  # koristimo list slicing koristeći negative indexing
+        clear_all()
+        display.insert(0, new_string)
+    else:
+        clear_all()
+        display.insert(0, "")
+
+
 # dodajemo display za unos u prvi red
-display = Entry(root)
+display = Entry(root, font=("default", 20))
 display.grid(row=1, columnspan=6)
 
 # dodajemo gumbe sa brojevima
@@ -65,6 +99,23 @@ for x in range(4):
             )
             button.grid(row=x + 2, column=y + 3, padx=1, pady=1)
             count += 1
+
+
+# dodajemo clear button u grid
+Button(root, text="AC", width=5, height=2, command=clear_all).grid(
+    row=5, column=0, padx=1, pady=1
+)
+
+# gumb izračunaj
+Button(root, text=" = ", width=5, height=2, command=calculate).grid(
+    row=5, column=2, padx=1, pady=1
+)
+
+# dodajemo gumb koji briše samo jednu znamenkuu unazad
+# ideja je uzeti cijeli string, izbristi iz displaya, maknuti zadnju znameku i vratiti smanjeni string u display
+Button(root, text=" <- ", width=5, height=2, command=lambda: undo()).grid(
+    row=5, column=5, padx=1, pady=1
+)
 
 
 root.mainloop()
