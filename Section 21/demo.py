1d0b3e5eec41bbaed93be95b2eaec6b7c2473004
Davor <davor.kos@gmail.com>
2024-12-30 00:03:45 +0100

    riješio pwrshl- install v7

--- Section 21/demo.py
+++ Section 21/demo.py
@@ -8,7 +8,10 @@ root = Tk()
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
 
-app_label = Label(root, text="QR Code Generator", fg='blue', font=('Arial', 30))
+app_label = Label(root, text="QR Code Generator", fg='blue', font=('Arial', 20))
 canvas.create_window(200,50, window=app_label)
+
+name_label= Label(root, text='Link name')
+
 
 root.mainloop()
