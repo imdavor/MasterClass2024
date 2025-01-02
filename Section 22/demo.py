873f73668c17a5a7c85061e685385dc60b06ba3d
Davor <davor.kos@gmail.com>
2025-01-02 15:47:36 +0100

    Building A Video Downloader App

--- Section 22/demo.py
+++ Section 22/demo.py
@@ -9,16 +9,23 @@ root.title("Video Downloader")
 canvas = Canvas(root, width=400, height=300)
 canvas.pack()
 
+# widget for entry to accept video url
 # app label
 app_label = Label(root, text="Video Downloader", fg="blue", font=("Arial", 20))
 canvas.create_window(200, 20, window=app_label)
 
 
-# widget for entry to accept video url
 url_label = Label(root, text="Enter video URL:")
 url_entry = Entry(root)
 canvas.create_window(200, 80, window=url_label)
 canvas.create_window(200, 100, window=url_entry)
+
+
+# widget for path  to download video
+path_label = Label(root, text="Select folder to download:")
+path_button = Button(root, text="Select")
+canvas.create_window(200, 140, window=path_label)
+canvas.create_window(200, 160, window=path_button)
 
 
 root.mainloop()
