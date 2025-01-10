e965574122bc5d29436bda8bb1417750da618183
Davor <davor.kos@gmail.com>
2025-01-10 19:22:10 +0100

    PDF

--- Section 24/demo.py
+++ Section 24/demo.py
@@ -1,13 +1,19 @@
 from fpdf import FPDF
 
 
-# customiziramo header
+# customiziramo i overridamo header
 class PDF(FPDF):
     def header(self):
-        self.image("images/logo.png", 100, 8, 75)
+        self.image("images/logo.png", 10, 8, 33)
         self.set_font("helvetica", "B", 16)
         self.cell(80)
-        self.cell(40, 10, "Hello World!")
+        self.cell(40, 10, "Hello World!", border=1, align="C")
+        self.ln(40)
+
+    def footer(self):
+        self.set_y(-15)
+        self.set_font("helvetica", "I", 16)
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
 
 
 # da bi koristio feature instaliranog paketa, moram kreirati objekt
@@ -15,5 +21,8 @@ pdf = PDF()
 
 # osnovne postavke pagea
 pdf.add_page()
+pdf.set_font("helvetica", "B", 16)
+for i in range(1, 41):
+    pdf.cell(0, 10, f"Printing line number {i}", new_x="LMARGIN", new_y="NEXT")
 
 pdf.output("pdf/sample.pdf")
