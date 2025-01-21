5eeeaf4229bb15e9627f09c5a80d320828e6dd65
Davor <davor.kos@gmail.com>
2025-01-21 23:50:31 +0100

    building a pdf invoice generator

---
+++ Section 25/demo.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+
+window = Tk()
+window.title("Invoice creator")
+
+medicine_label = Label(window, text="Medicine: ")
+medicine_label.pack()
+
+medicine_listbox = Listbox(window, selectmode="single")
+medicine_listbox.pack()
+
+quantity_label = Label(window, text="Quantity: ")
+quantity_label.pack()
+quantity_entry = Entry(window)
+quantity_entry.pack()
+
+
+add_button = Button(window, text="Add medicine: ")
+add_button.pack()
+
+
+total_amount_entry = label = Label(window, text="Total amount: ")
+total_amount_entry.pack()
+
+customer_label = Label(window, text="Customer name: ")
+customer_label.pack()
+customer_entry = Entry(window)
+customer_entry.pack()
+
+
+generate_button = Button(window, text="Generate Invoice")
+generate_button.pack()
+
+
+invoice_text = Text(window, height=10, width=50)
+invoice_text.pack()
+
+window.mainloop()
