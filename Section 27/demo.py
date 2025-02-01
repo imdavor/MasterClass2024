b068aaa26f78b817603201a217f6f86064cd8c90
Davor <davor.kos@gmail.com>
2025-02-02 00:09:29 +0100

    snake

--- Section 27/demo.py
+++ Section 27/demo.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -13,14 +14,20 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
-
 game_over = False
+score = 0
 
 x1 = window_width / 2
 y1 = window_height / 2
 
 x1_change = 0
 y1_change = 0
+
+length_of_snake = 1
+
+foodx = round(random.randrange(0, window_width - 10) / 10) * 10
+foody = round(random.randrange(0, window_height - 10) / 10) * 10
+
 
 clock = pygame.time.Clock()
 
@@ -49,9 +56,15 @@ while not game_over:
 
     if x1 > window_width or x1 < 0 or y1 > window_height or y1 < 0:
         game_over = True
-
     window.fill(black)
-    pygame.draw.rect(window, red, [400, 300, 10, 10])
+
+    if x1 == foodx and y1 == foody:
+        foodx = round(random.randrange(0, window_width - 10) / 10) * 10
+        foody = round(random.randrange(0, window_height - 10) / 10) * 10
+        length_of_snake += 1
+        score += 1
+
+    pygame.draw.rect(window, red, [foodx, foody, 10, 10])
     pygame.draw.rect(window, white, [x1, y1, 10, 10])
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(25)
