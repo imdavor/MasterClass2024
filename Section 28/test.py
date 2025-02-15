3cf28029eed01e85e16f1e9a1edbcf73eaa0e5de
Davor <davor.kos@gmail.com>
2025-02-16 00:37:47 +0100

    PyQt 378

--- Section 28/test.py
+++ Section 28/test.py
@@ -1,19 +1,7 @@
-from PyQt6.QtWidgets import (
-    QMainWindow,
-    QMessageBox,
-    QWidget,
-    QApplication,
-    QLabel,
-    QPushButton,
-    QLineEdit,
-    QCheckBox,
-    QHBoxLayout,
-    QVBoxLayout,
-    QGridLayout,
-)
+# Lecture: Calculate method
+# create a basic window
 import sys
-from PyQt6.QtGui import QPixmap, QFont
-import math
+from PyQt6.QtWidgets import QWidget, QApplication, QGridLayout, QLabel, QPushButton
 from PyQt6.QtCore import Qt
 
 
@@ -24,47 +12,68 @@ class Window(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Calculator")
-        # 12                +                   14
-        # previous input    current operator    current input
-        self.current_input = "0"
-        self.previous_input = ""
-        self.current_operator = ""
 
-        layout = QGridLayout()
-        self.setLayout(layout)
-        self.display = QLabel("0")
+        # Create a label to display the result
+        self.display = QLabel()
+        self.display.setText("0")
         self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
-        layout.addWidget(self.display, 0, 0, 1, 4)
+        # Now this widget is created, we need to add it to the layout given below
 
+        # Create number buttons and store those buttons inside an array
         self.buttons = [QPushButton(str(i)) for i in range(10)]
+        # Now add these buttons to the layout below
+        # Loop through every button and attach the number button clicked method to it
+        for button in self.buttons:
+            button.clicked.connect(self.number_button_clicked)
+
+        # Create the operator buttons
         self.operators = ["+", "-", "*", "/"]
         self.operator_buttons = [QPushButton(op) for op in self.operators]
+        # Now add these oeperator buttons to the layout below
         for button in self.operator_buttons:
             button.clicked.connect(self.operator_button_clicked)
 
+        # Create clear and equals button
         self.equals_button = QPushButton("=")
         self.equals_button.clicked.connect(self.calculate)
         self.clear_button = QPushButton("C")
 
+        # Create a layout and add it to the window
+        layout = QGridLayout()
+
+        # Add the display wudget to the layout
+        layout.addWidget(self.display, 0, 0, 1, 4)
+        # addWidget(widget,row,col,rowSpan,colSpan)
+
+        # adding the number buttons to the layout
+        # loop through each button first
+        # i gives the index value of a button
         for i, button in enumerate(self.buttons):
+            # divmod gives back the quotient and remainder
             row, col = divmod(i, 3)
+            # we divide the index of a button with 3 as we want to span 3 columns for button.
+            # if we want buttons to span 4 columns we divide by 4 in above divmod
             layout.addWidget(button, row + 1, col)
 
-        # adding event handling method to buttons
-        for button in self.buttons:
-            button.clicked.connect(self.number_button_clicked)
-
+        # Adding operator buttons
         for i, op_button in enumerate(self.operator_buttons):
             layout.addWidget(op_button, i + 1, 3)
 
-        layout.addWidget(self.equals_button, 4, 1)
+        # Add = and C button
         layout.addWidget(self.clear_button, 4, 2)
+        layout.addWidget(self.equals_button, 4, 1)
 
         self.setLayout(layout)
 
-    # metoda za hendlanje buttona sa brojevima
-    def number_button_clicked(self):
+        # Create three variables, current input, previous input and current operator
+        self.current_input = "0"
+        self.current_operator = ""
+        self.previous_operator = ""
 
+        # create a method for number button clicked
+
+    def number_button_clicked(self):
+        # get the text from the button being clicked
         digit = self.sender().text()
 
         if self.current_input == "0":
@@ -74,15 +83,18 @@ class Window(QWidget):
         self.display.setText(self.current_input)
 
     def operator_button_clicked(self):
-
+        # get the text from operator button
         operator = self.sender().text()
         if self.current_operator == "":
-            self.current_operator = operator
             self.previous_input = self.current_input
             self.current_input = "0"
+            self.current_operator = operator
+            print("current operator empty")
         else:
-            # calculate result
+            # call the calculate method here
+            print("current operator NOT empty")
             self.calculate()
+            # Same as above
             self.previous_input = self.current_input
             self.current_operator = operator
             self.current_input = "0"
