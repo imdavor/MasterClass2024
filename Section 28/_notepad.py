1594ff2a439d061a1016e49b60b54f8c1d968ffc
Davor <davor.kos@gmail.com>
2025-02-19 23:55:04 +0100

    notepad 390

--- Section 28/_notepad.py
+++ Section 28/_notepad.py
@@ -18,6 +18,7 @@ from PyQt6.QtWidgets import (
     QTextEdit,
     QMenuBar,
     QMenu,
+    QFileDialog,
 )
 import sys
 from PyQt6.QtGui import QPixmap, QFont, QAction, QIcon
@@ -51,8 +52,61 @@ class Window(QMainWindow):
         file_menu.addAction(new_action)
         new_action.triggered.connect(self.new_file)
 
+        open_action = QAction("Open", self)
+        file_menu.addAction(open_action)
+        open_action.triggered.connect(self.open_file)
+
+        save_action = QAction("Save", self)
+        file_menu.addAction(save_action)
+        save_action.triggered.connect(self.save_file)
+
+        save_as_action = QAction("Save as", self)
+        file_menu.addAction(save_as_action)
+        save_as_action.triggered.connect(self.save_as_file)
+
+        # edit menu
+        edit_menu = QMenu("Edit", self)
+        menubar.addMenu(edit_menu)
+
+        undo_action = QAction("Undo", self)
+        edit_menu.addAction(undo_action)
+        undo_action.triggered.connect(self.edit_field.undo)
+
+        redo_action = QAction("Redo", self)
+        edit_menu.addAction(redo_action)
+        redo_action.triggered.connect(self.edit_field.redo)
+
+        edit_menu.addSeparator()
+
+        cut_action = QAction("Cut", self)
+        edit_menu.addAction(cut_action)
+        cut_action.triggered.connect(self.edit_field.cut)
+
+        copy_action = QAction("Copy", self)
+        edit_menu.addAction(copy_action)
+        copy_action.triggered.connect(self.edit_field.copy)
+
+        paste_action = QAction("Paste", self)
+        edit_menu.addAction(paste_action)
+        paste_action.triggered.connect(self.edit_field.paste)
+
     def new_file(self):
         print("Creating New File")
+
+    def open_file(self):
+        print("Opening file")
+
+    def save_file(self):
+        print("Saving file")
+        file_path, _ = QFileDialog.getSaveFileName(
+            self, "Save File", "", "All Files(*);;Python File(*.py)"
+        )
+        if file_path:
+            with open(file_path, "w") as file:
+                file.write(self.edit_field.toPlainText())
+
+    def save_as_file(self):
+        print("Saving as...")
 
 
 app = QApplication(sys.argv)
